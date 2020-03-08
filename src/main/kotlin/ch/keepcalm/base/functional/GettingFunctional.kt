e1fdfb2086828e34824f5ba38238a9a5294bafcf
Marcel Widmer <marzelwidmer@gmail.com>
2020-03-08 07:43:07 +0100

    DSL

--- src/main/kotlin/ch/keepcalm/base/functional/GettingFunctional.kt
+++ src/main/kotlin/ch/keepcalm/base/functional/GettingFunctional.kt
@@ -5,16 +5,24 @@ fun operation(x: Int, y: Int, op: (Int, Int) -> Int): Int {
     return op(x, y)
 }
 
-fun operator(x: Int, op: (Int) -> Unit) {
-}
+fun operator(x: Int, op: (Int) -> Unit) {}
 
-fun route(path: String, vararg actions: (String, String) -> String) {
+fun route(path: String, vararg actions: (String, String) -> String) {}
 
-}
-
-
+fun unaryOperation(x: Int, op: (Int) -> Int) {}
+fun unaryOp(op: (Int) -> Int){}
 fun sum(x: Int, y: Int) = x + y
 
+fun transaction (db: Database, code: () -> Unit){
+    try {
+        code()
+    } finally {
+        db.commit()
+    }
+}
+class Database {
+    fun commit(){}
+}
 
 fun main() {
     val result = operation(x = 1, y = 2, op = ::sum) // ref function by name
@@ -27,4 +35,21 @@ fun main() {
 
     val sumLambda: (Int, Int) -> Int = { x, y -> x + y }
     operation(1, 2, sumLambda)
+
+    unaryOperation(2, { x -> x * x })
+    unaryOperation(3, { it * it })
+
+    unaryOperation(3) { it * it }
+
+    unaryOp { it * it }
+
+    unaryOp {
+        it * it
+    }
+
+    val db = Database()
+    transaction(db){
+        // Interact with database
+    }
+
 }
