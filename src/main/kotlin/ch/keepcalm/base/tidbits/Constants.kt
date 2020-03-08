807dd4db09037fd0aba53ca770eeb1daedae895e
Marcel Widmer <marzelwidmer@gmail.com>
2020-03-08 07:08:35 +0100

    Constant

---
+++ src/main/kotlin/ch/keepcalm/base/tidbits/Constants.kt
@@ -0,0 +1,12 @@
+package ch.keepcalm.base.tidbits
+
+val CopyrightAuthor = "John Doe"
+
+object Copyright {
+ val author = "John Doe"
+}
+
+fun main() {
+    CopyrightAuthor
+    Copyright.author
+}
